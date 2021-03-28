bd179b3183321413ec14573e870e95449f244983
rjmcgill <55286133+rjmcgill@users.noreply.github.com>
2021-03-28 15:19:41 -0400

    Added RecipeActivity scene

---
+++ app/src/main/java/com/example/comp3025_final_project/RecipeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.comp3025_final_project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecipeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe)
+    }
+}
