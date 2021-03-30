eb4994f4196e52a1149abe5a970c9c2826194250
rjmcgill <55286133+rjmcgill@users.noreply.github.com>
2021-03-30 05:47:00 -0400

    Final touches

---
+++ app/src/main/java/com/example/comp3025_final_project/SigninActivity.kt
@@ -0,0 +1,56 @@
+package com.example.comp3025_final_project
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+@Suppress("DEPRECATION")
+class SigninActivity : AppCompatActivity() {
+    val RC_SIGN_IN = 1234
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+
+        //From https://firebase.google.com/docs/auth/android/firebaseui?authuser=0#kotlin+ktx
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                Toast.makeText(this, "Sign in failed", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
